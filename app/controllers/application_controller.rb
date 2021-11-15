89405cc47615deb61c7ce526e7a77103feb32ab2
cabanga <manucabanga@gmail.com>
2021-11-15 10:06:27 +0100

    this commit - init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
     protected
         def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-                user_params.permit(:cell_phone, :email)
+                user_params.permit(:cell_phone, :email, :password)
             end
 
             #devise_parameter_sanitizer.permit(:sign_up, keys: [:cell_phone])
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,30 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+    before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+    # POST /resource/sign_in
+    def create
+        user = User.find_by_cell_phone(sign_in_params[:cell_phone])
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+        if user && user.valid_password?(sign_in_params[:password])
+            render json: {
+                messages: "Signed In Successfully",
+                is_success: true,
+                data: {user: user}
+            }, status: :ok
+        else
+            render json: {
+                messages: "Signed In Failed - Unauthorized",
+                is_success: false,
+                data: {}
+            }, status: :unauthorized
+        end
+    end
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    private
+        def configure_sign_in_params
+            params.require(:user).permit(:cell_phone, :password)
+        end
 end
