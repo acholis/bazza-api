8addb643f9687c94f649a91c5408a55f44601c14
cabanga <manucabanga@gmail.com>
2021-11-15 08:55:21 +0100

    this commit - add controllers user - devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-    devise_for :users
+    #devise_for :users
+    
+    devise_for :users, controllers: {
+        sessions: 'users/sessions'
+    }
 
     namespace :api do
         namespace :v1 do
