30443ce2fa85a5c8d28b272536699b4058103d44
cabanga <manucabanga@gmail.com>
2021-11-15 08:52:22 +0100

    this commit - init

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(
+    email: 'manucabanga@gmail.com',
+    cell_phone: '923747220',
+    password: 'q1w2e3r4', 
+    password_confirmation: 'q1w2e3r4'
+)
+
+p user
