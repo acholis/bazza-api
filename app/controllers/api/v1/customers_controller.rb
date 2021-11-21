85777aceebce452b4a0aa404b5d66ca56024746c
cabanga <manucabanga@gmail.com>
2021-11-21 16:26:46 +0100

    this commit - config customer

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,51 +1,51 @@
 class Api::V1::CustomersController < ApplicationController
-  before_action :set_api_v1_customer, only: [:show, :update, :destroy]
+    before_action :set_customer, only: [:show, :update, :destroy]
 
-  # GET /api/v1/customers
-  def index
-    @api_v1_customers = Api::V1::Customer.all
+    # GET /api/v1/customers
+    #def index
+    #    @api_v1_customers = Api::V1::Customer.all
 
-    render json: @api_v1_customers
-  end
+    #    render json: @api_v1_customers
+    #end
 
-  # GET /api/v1/customers/1
-  def show
-    render json: @api_v1_customer
-  end
+    ## GET /api/v1/customers/1
+    #def show
+    #    render json: @api_v1_customer
+    #end
 
-  # POST /api/v1/customers
-  def create
-    @api_v1_customer = Api::V1::Customer.new(api_v1_customer_params)
+    ## POST /api/v1/customers
+    #def create
+    #    @api_v1_customer = Api::V1::Customer.new(api_v1_customer_params)
 
-    if @api_v1_customer.save
-      render json: @api_v1_customer, status: :created, location: @api_v1_customer
-    else
-      render json: @api_v1_customer.errors, status: :unprocessable_entity
-    end
-  end
+    #    if @api_v1_customer.save
+    #    render json: @api_v1_customer, status: :created, location: @api_v1_customer
+    #    else
+    #    render json: @api_v1_customer.errors, status: :unprocessable_entity
+    #    end
+    #end
 
-  # PATCH/PUT /api/v1/customers/1
-  def update
-    if @api_v1_customer.update(api_v1_customer_params)
-      render json: @api_v1_customer
-    else
-      render json: @api_v1_customer.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /api/v1/customers/1
-  def destroy
-    @api_v1_customer.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_api_v1_customer
-      @api_v1_customer = Api::V1::Customer.find(params[:id])
+    # PATCH/PUT /api/v1/customers/1
+    def update
+        if @customer.update(customer_params)
+            render json: @customer
+        else
+            render json: @customer.errors, status: :unprocessable_entity
+        end
     end
 
-    # Only allow a list of trusted parameters through.
-    def api_v1_customer_params
-      params.fetch(:api_v1_customer, {})
+    # DELETE /api/v1/customers/1
+    def destroy
+        @customer.destroy
     end
+
+    private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_customer
+            @customer = Customer.find(params[:id])
+        end
+
+        # Only allow a list of trusted parameters through.
+        def customer_params
+            params.require(:customer).permit(:first_name, :last_name)
+        end
 end
